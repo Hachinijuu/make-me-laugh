614cd9ab0d662815f77bf3b163cdf6d2f270decd
BrenBoy1506 <86203477+BrenBoy1506@users.noreply.github.com>
2024-01-28 14:30:46 -0500

    Update Pog

--- Epic Poggers Jam Of Game/Assets/Scripts/PlayerMovement.cs
+++ Epic Poggers Jam Of Game/Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float movespeed;
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
 
 
     // Update is called once per frame
@@ -13,21 +19,30 @@ public class PlayerMovement : MonoBehaviour
         if(Input.GetKey(KeyCode.W)) 
         {
             transform.Translate((Vector2.up * movespeed)* Time.deltaTime);
+            animator.SetInteger("moveState", 2);
         }
 
         if (Input.GetKey(KeyCode.S))
         {
             transform.Translate((Vector2.down * movespeed) * Time.deltaTime);
+            animator.SetInteger("moveState", 1);
         }
 
         if (Input.GetKey(KeyCode.A))
         {
             transform.Translate((Vector2.left * movespeed) * Time.deltaTime);
+            animator.SetInteger("moveState", 3);
         }
 
         if (Input.GetKey(KeyCode.D))
         {
             transform.Translate((Vector2.right * movespeed) * Time.deltaTime);
+            animator.SetInteger("moveState", 4);
+        }
+
+        if(!Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.D)) 
+        {
+            animator.SetInteger("moveState", 0);
         }
     }
 }
